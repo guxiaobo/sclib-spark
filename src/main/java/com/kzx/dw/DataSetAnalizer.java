af73893e393fbac17b088ac97773114f60b410ae
tovin-xu <xutaota2003@163.com>
2014-07-07 15:45:52 +0800

    create DataSetAnalizer、DataSetTransformer class

---
+++ src/main/java/com/kzx/dw/DataSetAnalizer.java
@@ -0,0 +1,11 @@
+/**
+* @Title: DataSetAnalizer.java
+* @Description: 数据集通用分析处理类
+* @author tovin/xutaota2003@163.com 
+* @date 2014年7月7日 下午3:37:48 
+ */
+package com.kzx.dw;
+public class DataSetAnalizer {
+
+}
+
---
+++ src/main/java/com/kzx/dw/DataSetTransformer.java
@@ -0,0 +1,11 @@
+/**
+* @Title: DataSetTransformer.java
+* @Description: 数据集通用转化类
+* @author tovin/xutaota2003@163.com 
+* @date 2014年7月7日 下午3:38:54 
+ */
+package com.kzx.dw;
+public class DataSetTransformer {
+
+}
+
